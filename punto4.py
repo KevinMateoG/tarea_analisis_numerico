e2349ec8f38f29f2567fa74dcd586db28f62c782
Kevin Mateo Gomez Diez <gomezdiezkevinmateo@gmail.com>
2026-03-09 13:43:29 -0500

    ayuda

--- punto4.py
+++ punto4.py
@@ -1,7 +1,10 @@
 from ceros_de_funcion import *
-
+import numpy as np
+import matplotlib.pyplot as plt
 #Metodo biseccion
 
 f= lambda s: (s**4) + (20.75) * s**3 + (92.6) * s**2 + (73.69) * s
 
-print(biseccion(f, -2, 0,1e-4))
+ux = np.linspace(0,10,100)
+plt.plot(ux, f(ux))
+plt.axhline(0,linestyle="--", color='red')
